6a751123ba1634c870488aa38f5f876f2795e8bb
ranga <rangareddyjava9@gmail.com>
2020-02-05 10:35:23 +0530

    first commit

--- src/main/java/com/ranga/spring/web/fileio/FileIoApplication.java
+++ src/main/java/com/ranga/spring/web/fileio/FileIoApplication.java
@@ -2,6 +2,7 @@ package com.ranga.spring.web.fileio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.unit.DataSize;
@@ -14,47 +15,12 @@ public class FileIoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileIoApplication.class, args);
 	}
-	/*@Bean
-	public TomcatServletWebServerFactory containerFactory() {
-		return new TomcatServletWebServerFactory() {
-			protected void customizeConnector(Connector connector) {
-				int maxSize = 500000000;
-				super.customizeConnector(connector);
-				connector.setMaxPostSize(maxSize);
-				connector.setMaxSavePostSize(maxSize);
-				if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
-
-					((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
-					logger.info("Set MaxSwallowSize "+ maxSize);
-				}
-			}
-		};
-
-	}*/
-	//Tomcat large file upload connection reset
-	//http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
-	/*@Bean
-	@Primary
-	public TomcatServletWebServerFactory tomcatEmbedded() {
-
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		//tomcat.getTomcatConnectorCustomizers().clear();
-		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
-			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
-				connector.setMaxPostSize(-1);
-				connector.setMaxSavePostSize(-1);
-			}
-		});
-
-		return tomcat;
-
-	}*/
 	@Bean
 	MultipartConfigElement multipartConfigElement() {
 		MultipartConfigFactory factory = new MultipartConfigFactory();
-		factory.setMaxFileSize(DataSize.ofBytes(512000000L));
-		factory.setMaxRequestSize(DataSize.ofBytes(512000000L));
-		factory.setFileSizeThreshold(DataSize.ofBytes(512000000L));
+		factory.setMaxFileSize(DataSize.ofBytes(51200000000000L));
+		factory.setMaxRequestSize(DataSize.ofBytes(51200000000000L));
+		factory.setFileSizeThreshold(DataSize.ofBytes(51200000000000L));
 		return factory.createMultipartConfig();
 	}
 }
--- src/main/java/com/ranga/spring/web/fileio/FileUploadController.java
+++ src/main/java/com/ranga/spring/web/fileio/FileUploadController.java
@@ -1,10 +1,12 @@
 package com.ranga.spring.web.fileio;
 
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
@@ -12,27 +14,22 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
-@Controller
+@RestController
+@Scope(value = "singleton")
 public class FileUploadController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public @ResponseBody
-    Response<String> upload(HttpServletRequest request) throws IOException {
-
+    public @ResponseBody Response<String> upload(HttpServletRequest request) throws IOException {
         boolean isMultipart = ServletFileUpload.isMultipartContent(request);
         if (!isMultipart) {
             // Inform user about invalid request
             Response<String> responseObject = new Response<String>(false, "Not a multipart request.", "");
             return responseObject;
         }
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload();
-
         // Parse the request
         MultipartFile multipartFile = ((StandardMultipartHttpServletRequest) request).getMultiFileMap().getFirst("filevideo");
-        File file=new File(this.getClass().getResource("/").getFile().concat(Long.toString(System.nanoTime())).concat(".pdf"));
+        File file=new File(this.getClass().getResource("/").getFile().concat(Long.toString(System.nanoTime())).concat(".mp4"));
         file.createNewFile();
         multipartFile.transferTo(file);
-
         return new Response<String>(true, "Success", "");
     }
 }
